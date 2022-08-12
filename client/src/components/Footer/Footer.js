85e635a2111abf47994d125b8d996217b4553c3b
saagish <vikke07071985@gmail.com>
2022-08-12 11:03:31 +0530

    design

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,154 @@
+import React from 'react'
+
+
+function Footer() {
+    return(
+//     <div >
+//           <footer class="footer">
+//   <div class="footer__addr">
+//     <h1 class="footer__logo">Something</h1>
+        
+//     <h2>Contact</h2>
+    
+//     <address>
+//       5534 Somewhere In. The World 22193-10212<br></br>
+          
+//       <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
+//     </address>
+//   </div>
+  
+//   <ul class="footer__nav">
+//     <li class="nav__item">
+//       <h2 class="nav__title">Media</h2>
+
+//       <ul class="nav__ul">
+//         <li>
+//           <a href="#">Online</a>
+//         </li>
+
+//         <li>
+//           <a href="#">Print</a>
+//         </li>
+            
+//         <li>
+//           <a href="#">Alternative Ads</a>
+//         </li>
+//       </ul>
+//     </li>
+    
+//     <li class="nav__item nav__item--extra">
+//       <h2 class="nav__title">Technology</h2>
+      
+//       <ul class="nav__ul nav__ul--extra">
+//         <li>
+//           <a href="#">Hardware Design</a>
+//         </li>
+        
+//         <li>
+//           <a href="#">Software Design</a>
+//         </li>
+        
+//         <li>
+//           <a href="#">Digital Signage</a>
+//         </li>
+        
+//         <li>
+//           <a href="#">Automation</a>
+//         </li>
+        
+//         <li>
+//           <a href="#">Artificial Intelligence</a>
+//         </li>
+        
+//         <li>
+//           <a href="#">IoT</a>
+//         </li>
+//       </ul>
+//     </li>
+    
+//     <li class="nav__item">
+//       <h2 class="nav__title">Legal</h2>
+      
+//       <ul class="nav__ul">
+//         <li>
+//           <a href="#">Privacy Policy</a>
+//         </li>
+        
+//         <li>
+//           <a href="#">Terms of Use</a>
+//         </li>
+        
+//         <li>
+//           <a href="#">Sitemap</a>
+//         </li>
+//       </ul>
+//     </li>
+//   </ul>
+  
+//   <div class="legal">
+//     <p>&copy; 2019 Something. All rights reserved.</p>
+    
+//     <div class="legal__links">
+//       <span>Made by <span class="heart">♥</span> Sriram</span>
+//     </div>
+//   </div>
+// </footer>
+//     </div>
+  
+<div class="footer__container">
+<div class="footer__links">
+  <div class="footer__link--wrapper">
+    <div class="footer__link--items">
+      <h2>About Us</h2>
+      <a href="/sign__up">How it works</a> <a href="/">Testimonials</a>
+      <a href="/">Careers</a> <a href="/">Terms of Service</a>
+    </div>
+    <div class="footer__link--items">
+      <h2>Contact Us</h2>
+      <a href="/">Contact</a> <a href="/">Support</a>
+      <a href="/">Destinations</a>
+    </div>
+  </div>
+  <div class="footer__link--wrapper">
+    <div class="footer__link--items">
+      <h2>Videos</h2>
+      <a href="/">Submit Video</a> <a href="/">Ambassadors</a>
+      <a href="/">Agency</a>
+    </div>
+    <div class="footer__link--items">
+      <h2>Social Media</h2>
+      <a href="/">Instagram</a> <a href="/">Facebook</a>
+      <a href="/">Youtube</a> <a href="/">Twitter</a>
+    </div>
+  </div>
+</div>
+<section class="social__media">
+  <div class="social__media--wrap">
+    <div class="footer__logo">
+      <a href="/" id="footer__logo">   </a>
+    </div>
+    <p class="website__rights">© COLOR 2020. All rights reserved</p>
+    <div class="social__icons">
+      <a href="/" class="social__icon--link" target="_blank"
+        ><i class="fab fa-facebook"></i
+      ></a>
+      <a href="/" class="social__icon--link"
+        ><i class="fab fa-instagram"></i
+      ></a>
+      <a href="/" class="social__icon--link"
+        ><i class="fab fa-youtube"></i
+      ></a>
+      <a href="/" class="social__icon--link"
+        ><i class="fab fa-linkedin"></i
+      ></a>
+      <a href="/" class="social__icon--link"
+        ><i class="fab fa-twitter"></i
+      ></a>
+    </div>
+  </div>
+</section>
+</div>
+)
+}
+
+export default Footer
